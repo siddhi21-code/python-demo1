76b8fb30f7e0cb6a652d9d0ca100bcb01556cd0b
siddhi21-code <siddhishirke508@gmail.com>
2025-09-04 16:47:31 +0530

    ok

---
+++ demo1.py
@@ -0,0 +1,16 @@
+name =input("enter your name ")
+age = int(input("enter  your  age "))
+
+
+
+print("hi sidd")
+print (1234)
+print(20-12)
+print(20*1)
+print("python is easy language ")
+
+
+
+
+
+
